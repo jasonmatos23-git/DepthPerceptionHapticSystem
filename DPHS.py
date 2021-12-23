7bdf21f446897983dea69f01c36c776f03435c35
Cristopher Matos <jasonmatos23@gmail.com>
2021-12-23 12:30:40 -0500

    Modify author name of DPHS

--- DPHS.py
+++ DPHS.py
@@ -1,4 +1,4 @@
-# Primary Author:	<FirstName LastName (email)>
+# Primary Author:	Cristopher Matos (jasonmatos23@gmail.com)
 # Course:			EEL 4915L Senior Design II, UCF Fall 2020
 # File name:		DHPS.py
 # Description:		Contains the entire system and system mode.
