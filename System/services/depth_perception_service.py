6766c1894dd96811999d66bad08b9c1a95aebb8d
Cristopher Matos <jasonmatos23@gmail.com>
2021-12-25 10:38:01 -0500

    Create DepthPerceptionService base code

---
+++ System/services/depth_perception_service.py
@@ -0,0 +1,18 @@
+# Primary Author:	Cristopher Matos (jasonmatos23@gmail.com)
+# Course:			EEL 4915L Senior Design II, UCF Spring 2022
+# File name:		depth_perception_service.py
+# Description:		Apply depth perception TFLite model
+
+# from system.models.service import Service
+# from system.models.routine import Routine
+# from system.API.Input import Input
+# from system.routine_container import RoutineContainer
+
+class DepthPerceptionService :
+
+	# def __init__(self, input_: Input, routineContainer: RoutineContainer) -> None:
+	# 	self.input_: Input = input_
+	# 	self.routine: Routine = routineContainer.GetRoutine("TemplateRoutine")
+
+	def Execute(self) -> None:
+		pass	# Code to run model
