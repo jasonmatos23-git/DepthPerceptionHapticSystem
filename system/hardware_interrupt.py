aff4da63883ce13ba7252597035c253313e9f03a
Cristopher Matos <jasonmatos23@gmail.com>
2021-12-24 20:05:45 -0500

    Adding empty scheduler and hardware_interrupt files

---
+++ system/hardware_interrupt.py
@@ -0,0 +1,4 @@
+# Primary Author:	<FirstName LastName (email)>
+# Course:			EEL 4915L Senior Design II, UCF Spring 2022
+# File name:		hardware_interrupt.py
+# Description:		Connects routines to GPIO events.
---
+++ system/scheduler.py
@@ -0,0 +1,4 @@
+# Primary Author:	<FirstName LastName (email)>
+# Course:			EEL 4915L Senior Design II, UCF Spring 2022
+# File name:		scheduler.py
+# Description:		Puts services on some schedule.
