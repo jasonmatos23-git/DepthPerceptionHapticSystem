523f0dc50fcff0e9df09e4fed9f7a9aacc9648f6
Cristopher Matos <jasonmatos23@gmail.com>
2022-01-05 10:25:07 -0500

    Clean depthmodel reduces output size to 4x4 px

--- system/services/depthmodel/depthmodel.py
+++ system/services/depthmodel/depthmodel.py
@@ -9,40 +9,49 @@
 
 import cv2
 import tflite_runtime.interpreter as tflite
-from numpy import float32 as float32, uint8 as uint8, empty as empty
+from numpy import float32, uint8, empty, exp
 
 class DepthModel :
 
+	# 2D Gaussian function with std_dev of 70 px and peak at 128
+	def _Gaussian2D(self, x, y) :
+		var = 70**2
+		pk = 128
+		return exp(-(((x-pk)**2)/(2*var)+((y-pk)**2)/(2*var)))
+
+	# 2D Gaussian filter for reducing effect of gradient on
+	# depth estimation
+	def _Gaussian2DFilter(self) :
+		filt = empty([256, 256])
+		for i in range(0, 256) :
+			for j in range(0, 256) :
+				filt[i, j] = self._Gaussian2D(i, j)
+		return filt
+
 	def __init__(self) :
-		self.interpreter = tflite.Interpreter(model_path="model_opt.tflite")
-		self.mean = [0.485, 0.456, 0.406]
-		self.std = [0.229, 0.224, 0.225]
+		self._interpreter = tflite.Interpreter(model_path="model_opt.tflite")
+		self._mean = [0.485, 0.456, 0.406]
+		self._std = [0.229, 0.224, 0.225]
 
+	# Get depth map from an image
 	def RunInference(self, img) :
-		cv2.imwrite("input.png", cv2.cvtColor(img, cv2.COLOR_RGB2BGR))	# Writes input image for demonstration
-
-		# input
+		# Create input tensor
 		img = img / 255.0
 		img_input = cv2.resize(img, (256,256), interpolation=cv2.INTER_CUBIC)
-		img_input = (img_input - self.mean) / self.std
+		img_input = (img_input - self._mean) / self._std
 		reshape_img = img_input.reshape(1,256,256,3)
 		tensor = reshape_img.astype(float32)
-
-		# load model
-		self.interpreter.allocate_tensors()
-		input_details = self.interpreter.get_input_details()
-		output_details = self.interpreter.get_output_details()
-		input_shape = input_details[0]['shape']
-
-		# inference
-		self.interpreter.set_tensor(input_details[0]['index'], tensor)
-		self.interpreter.invoke()
-		output = self.interpreter.get_tensor(output_details[0]['index'])
+		# Load model
+		self._interpreter.allocate_tensors()
+		input_details = self._interpreter.get_input_details()
+		output_details = self._interpreter.get_output_details()
+		# Inference
+		self._interpreter.set_tensor(input_details[0]['index'], tensor)
+		self._interpreter.invoke()
+		output = self._interpreter.get_tensor(output_details[0]['index'])
 		output = output.reshape(256, 256)
-
-		# output file
-		#prediction = cv2.resize(output, (img.shape[1], img.shape[0]), interpolation=cv2.INTER_CUBIC)
-		prediction = output
+		# Downsizing result
+		prediction = cv2.resize(output, (4, 4), interpolation=cv2.INTER_NEAREST)
 		depth_min = prediction.min()
 		depth_max = prediction.max()
 		# LiDAR measurement may be useful for following line
