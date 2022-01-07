5d3f0a166e61c4eaf54ecc6fee88c1d2b53fdac5
Cristopher Matos <jasonmatos23@gmail.com>
2022-01-06 19:25:51 -0500

    Testing with 24x16 resolution

--- system/services/depth_perception_service.py
+++ system/services/depth_perception_service.py
@@ -16,7 +16,7 @@ from time import sleep
 class DepthPerceptionService :
 
 	def __init__(self, input_: Input, routineContainer: RoutineContainer) -> None:
-		self._depthModel = DepthModel((3, 2))
+		self._depthModel = DepthModel((24, 16))
 		self.input_: Input = input_
 		self.routine: Routine = routineContainer.GetRoutine("DepthPerceptionRoutine")
 
