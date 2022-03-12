2e3927b91e651c32b4f14bf272f5bdf3b5fb54d6
Cristopher Matos <jasonmatos23@gmail.com>
2022-03-12 11:29:57 -0500

    Update input component to use LiDAR class

--- system/API/Input.py
+++ system/API/Input.py
@@ -2,21 +2,14 @@
 # Course:			EEL 4915L Senior Design II, UCF Spring 2022
 # File name:		Input.py
 # Description:		Connects input hardware to software components.
-# Note:				TFMini-S MUST be configured to I2C mode over UART first.
 
 # Imports for camera
 from picamera import PiCamera
 from numpy import empty, ndarray, uint8
 from time import sleep
-from smbus2 import SMBus, i2c_msg
+from LiDAR import LiDAR
 
 class Input :
-
-	# Class variables
-	_forwardLidarAddress: int = 0x10
-	_reqForwardLidar: i2c_msg = i2c_msg.write(_forwardLidarAddress, \
-		[0x5A, 0x05, 0x00, 0x01, 0x60]) # Benewake TFMini-S command to read distance (cm)
-	_resForwardLidar: i2c_msg = i2c_msg.read(_forwardLidarAddress, 9) # 9 result bytes
 
 	# INITIATION FUNCTIONS
 
@@ -27,12 +20,9 @@ class Input :
 		sleep(2)	# Camera setup time
 		return camera
 
-	def _initForwardLidar(self) -> SMBus:
-		return SMBus(1)	# Hardcoded I2C bus 1 for TESTING
-
 	def __init__(self) -> None:
 		self._camera: PiCamera = self._initCamera()
-		self._forwardLidar: SMBus = self._initForwardLidar()
+		self._lidar: LiDAR = LiDAR()
 
 	# DESTRUCTION FUNCTIONS
 
@@ -41,14 +31,9 @@ class Input :
 			self._camera.close()
 			self._camera = None
 
-	def _delForwardLidar(self) -> None:
-		if not (self._forwardLidar is None) :
-			self._forwardLidar.close()
-			self._forwardLidar = None
-
 	def __del__(self) -> None:
 		self._delCamera()
-		self._delForwardLidar()
+		del(self._lidar)
 
 	# PUBLIC FUNCTIONS
 
@@ -65,26 +50,9 @@ class Input :
 		self._camera.capture(img, format="rgb", use_video_port=True)
 		return img.reshape((self._camera.resolution[1], self._camera.resolution[0], 3))
 
-	# Forward lidar
-	def _getForwardLidarRaw(self) -> None:
-		self._forwardLidar.i2c_rdwr(self._reqForwardLidar)
-		sleep(0.001)	# Recommended wait time for result
-		self._forwardLidar.i2c_rdwr(self._resForwardLidar)
+	# Lidar
+	def GetForwardLidar(self) -> int:
+		return self._lidar.GetForwardLidar()
 
-	def _filteredForwardLidar(self) -> int:
-		self._getForwardLidarRaw()
-		res: i2c_msg = self._resForwardLidar
-		return (256 * int.from_bytes(res.buf[3], "big")) + int.from_bytes(res.buf[2], "big")
-
-	def GetForwardLidar(self) -> list:
-		result: int = self._filteredForwardLidar()
-		if result >= 0 :
-			return result
-		elif result == -1 :
-			raise RuntimeError("Unstable signal (strength < 100)")
-		elif result == -2 :
-			raise RuntimeError("Signal strength saturation")
-		elif result == -4 :
-			raise RuntimeError("Ambient light saturation")
-		else :
-			raise RuntimeError("Unknown error in LiDAR result")
+	def GetAngledLidar(self) -> int:
+		return self._lidar.GetAngledLidar()
