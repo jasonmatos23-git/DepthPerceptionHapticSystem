719796ceba30cafa61003110d1f9bc2b0daa2996
Cristopher Matos <jasonmatos23@gmail.com>
2022-03-28 11:19:22 -0400

    Poll modes in scheduler

--- system/scheduler.py
+++ system/scheduler.py
@@ -5,7 +5,7 @@
 
 from system.service_container import ServiceContainer
 from system.models.service import Service
-from system.mode import State
+from system.mode import State, DPHSMode
 
 from time import sleep
 
@@ -13,10 +13,26 @@ class Scheduler :
 
 	def __init__(self, serviceContainer: ServiceContainer, state: State) :
 		self.state: State = state
+		self.prevMode: DPHSMode = self.state.getMode()
 		self.depthService = serviceContainer.GetService("DepthPerceptionService")
 
 	def Run(self) :
+		# Mode change indications should also be called here.
+		# For general and outdoor mode, service will wrap a routine
+		# which will play enalbed/disabled indicator.
+		# LPM does that, and also enables/disables low power modes
+		# of hardware.
+		# Note: previous mode tracked to know when a mode change occurs
+		# to play indicator. In LPM case, also used as a debounce for enabling
+		# or disabling mode.
 		while(True) :
-			self.depthService.Execute()
-			sleep(0.3)
+			currentMode: DPHSMode = self.state.getMode()
+			if currentMode == DPHSMode.LOW_POWER :
+				continue	# Toggle LPM
+			elif currentMode == DPHSMode.GENERAL :
+				self.depthService.Execute()
+				sleep(0.3)	# Sleep unnecessary, used for demonstrations
+				continue
+			elif currentMode == DPHSMode.OUTDOOR :
+				continue	# Run depth + curb
 
