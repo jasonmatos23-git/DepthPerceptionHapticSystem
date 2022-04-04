79982d859c92720163cdc69a6054a980669a0f60
Cristopher Matos <jasonmatos23@gmail.com>
2022-04-04 14:39:44 -0400

    Button response callback functions

--- system/routines/button_response.py
+++ system/routines/button_response.py
@@ -6,6 +6,7 @@
 from system.models.routine import Routine
 from system.mode import State
 from system.API.Output import Output
+from system.mode import DPHSMode
 
 class ButtonResponse(Routine) :
 
@@ -16,4 +17,22 @@ class ButtonResponse(Routine) :
 	# On button press, should play an indication that a button
 	# press was registered, and change the state.
 	def Execute(self) -> None:
+		# Respond to general button press (speaker and/or haptic output)
+		print("Button press received.")
+
+	# Callback functions
+	def Button1Down(self) :
+		self.Execute()
+		self.state.setMode(DPHSMode.GENERAL)
+
+	def Button2Down(self) :
+		pass
+
+	def Button3Down(self) :
+		pass
+
+	def Button4Down(self) :
+		pass
+
+	def Button5Down(self) :
 		pass
