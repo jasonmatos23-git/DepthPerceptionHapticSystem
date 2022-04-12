c5db29134b85b898f45e88248d8bd5b01e6fcab8
Christa Lawrence <cal47day@gmail.com>
2022-04-12 17:14:48 -0400

    outdoor mode-curb

---
+++ system/modes/outdoor.py
@@ -0,0 +1,31 @@
+# Primary Author:	Cristopher Matos (jasonmatos23@gmail.com)
+# Course:			EEL 4915L Senior Design II, UCF Spring 2022
+# File name:		outdoor.py
+# Description:		Define outdoor mode.
+
+from threading import Event
+from system.models.mode import Mode
+from system.models.service import Service
+from system.service_container import ServiceContainer
+from system.modes.general import GeneralMode
+
+from time import sleep
+
+class OutdoorMode(Mode) :
+
+	def __init__(self, modeChangedEvent: Event, serviceContainer: ServiceContainer, generalMode: GeneralMode) :
+		self._modeChangedEvent: Event = modeChangedEvent
+		self._generalMode: GeneralMode = generalMode
+        self.curbService: Service = serviceContainer.GetService("CurbDetectionService")
+
+	def enter(self) -> None:
+		pass
+
+	def exit(self) -> None:
+		pass
+
+	def Execute(self) -> None:
+		while not self._modeChangedEvent.is_set() :
+			self._generalMode.Execute()
+            self._curbService.Execute()
+			# TODO: Execute curb detection service
