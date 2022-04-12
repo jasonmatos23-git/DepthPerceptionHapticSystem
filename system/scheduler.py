1840b398af10b6ecd254b3505f18ae4be21e1059
Cristopher Matos <jasonmatos23@gmail.com>
2022-04-11 23:27:53 -0400

    Update init of scheduler to take in System instance

--- system/scheduler.py
+++ system/scheduler.py
@@ -1,23 +1,23 @@
-# Primary Author:	<FirstName LastName (email)>
+# Primary Author:	Cristopher Matos (jasonmatos23@gmail.com)
 # Course:			EEL 4915L Senior Design II, UCF Spring 2022
 # File name:		scheduler.py
 # Description:		Puts services on some schedule.
 
-from system.service_container import ServiceContainer
 from system.models.service import Service
 from system.mode import State, DPHSMode
 from system.models.mode import Mode
+from system.system import System
 from system.modes.general import GeneralMode
 from system.modes.lowpower import LowPowerMode
 from system.modes.outdoor import OutdoorMode
 
 class Scheduler :
 
-	def __init__(self, serviceContainer: ServiceContainer, state: State) :
-		self.state: State = state
-		self.generalMode: Mode = GeneralMode(state.modeChangedEvent, serviceContainer)
-		self.lowpowerMode: Mode = LowPowerMode(state.modeChangedEvent, \
-			serviceContainer.input_, serviceContainer.routineContainer.output_)
+	def __init__(self, DPHS: System) :
+		self.state: State = DPHS.state
+		self.generalMode: Mode = GeneralMode(self.state.modeChangedEvent, DPHS.serviceContainer)
+		self.lowpowerMode: Mode = LowPowerMode(self.state.modeChangedEvent, \
+			DPHS.serviceContainer.input_, DPHS.routineContainer.output_)
 
 	# NOTE: If calling Run() after the initial call ensure that
 	#		the state is changed via setMode() to a mode other than
@@ -30,13 +30,10 @@ class Scheduler :
 			if currentMode == DPHSMode.LOW_POWER :
 				self.lowpowerMode.Execute()
 			elif currentMode == DPHSMode.GENERAL :
-				self.generalMode.enter()
 				self.generalMode.Execute()
-				self.generalMode.exit()
 			elif currentMode == DPHSMode.OUTDOOR :
 				pass	# Execute outdoor
 			elif currentMode == DPHSMode.EXIT :
 				self.state.modeChangedEvent.clear()
 				break
 			self.state.modeChangedEvent.clear()
-
