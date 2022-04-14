2cabb2c2f25174939f0f4d76d95f1a028c2339b7
Cristopher Matos <jasonmatos23@gmail.com>
2022-04-14 17:39:24 -0400

    Outdoor mode fixing

--- system/modes/outdoor.py
+++ system/modes/outdoor.py
@@ -8,22 +8,13 @@ from system.models.mode import Mode
 from system.models.service import Service
 from system.service_container import ServiceContainer
 from system.modes.general import GeneralMode
-<<<<<<< HEAD
-
-=======
->>>>>>> dev/v1-0-0
-from time import sleep
 
 class OutdoorMode(Mode) :
 
 	def __init__(self, modeChangedEvent: Event, serviceContainer: ServiceContainer, generalMode: GeneralMode) :
 		self._modeChangedEvent: Event = modeChangedEvent
 		self._generalMode: GeneralMode = generalMode
-<<<<<<< HEAD
-                self._curbService: Service = serviceContainer.GetService("CurbDetectionService")
-=======
-		# TODO: Pass in curb detection
->>>>>>> dev/v1-0-0
+		self._curbService: Service = serviceContainer.GetService("CurbDetectionService")
 
 	def enter(self) -> None:
 		pass
@@ -34,9 +25,4 @@ class OutdoorMode(Mode) :
 	def Execute(self) -> None:
 		while not self._modeChangedEvent.is_set() :
 			self._generalMode.Execute()
-<<<<<<< HEAD
-                self._curbService.Execute()
-			# TODO: Execute curb detection service
-=======
-			# TODO: Execute curb detection service
->>>>>>> dev/v1-0-0
+			self._curbService.Execute()
